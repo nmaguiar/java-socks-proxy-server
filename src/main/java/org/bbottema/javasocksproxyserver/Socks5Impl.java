41115872482c0baf1bfc82bced6838c68a30ee6a
Nuno Aguiar <nmaguiar@users.noreply.github.com>
2025-07-18 19:56:18 +0000

    Refactor POM and update Socks5Impl imports for consistency

--- src/main/java/org/bbottema/javasocksproxyserver/Socks5Impl.java
+++ src/main/java/org/bbottema/javasocksproxyserver/Socks5Impl.java
@@ -1,10 +1,10 @@
 package org.bbottema.javasocksproxyserver;
 
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.InterruptedIOException;
 import java.net.DatagramPacket;
